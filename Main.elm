c1d219177b647df9fdd4c21c6a62269d19b3c038
Andrew Miller <ak.miller@auckland.ac.nz>
2013-07-03 11:00:49 +1200

    Get a basic cube visualisation with lighting working well.

--- Main.elm
+++ Main.elm
@@ -1,46 +1,264 @@
-import Native.Graphics.WebGLMesh as N
+import Native.Graphics.WebGLScene as N
 import Dict as D
+import Mouse
+import Keyboard
+import Signal
 
 data BasisFunction = CONSTANT | LINEAR_LAGRANGE | QUADRATIC_LAGRANGE |
                      CUBIC_LAGRANGE | LINEAR_SIMPLEX | QUADRATIC_SIMPLEX
 type Element = Int
 data Coord3D = Coord3D Float Float Float
-data Triangle = Triangle Element Element Element
+data Triangle = Triangle Coord3D Coord3D Coord3D
 data Shape = LINE | SQUARE | TRIANGLE | CUBE | TETRAHEDRON | WEDGE12 | WEDGE13 | WEDGE23
 type GLProjection = Matrix4x4
+data GLColour = GLColour Float Float Float
 data Matrix4x4 = Matrix4x4 [Float]
+data Quaternion = Quaternion (Float, Float, Float, Float)
 
-data GLMesh = GLMesh {
-  -- A list of node positions.
-  nodalPositions: [Coord3D],
-  -- A list of node indices per element (first node is 1).
-  elements: [Triangle]
+data GLPrimModel = GLPrimModel {
+  -- A list of primitive triangles.
+  primitives: [Triangle]
+  }
+
+data GLPrimSceneView = GLPrimSceneView {
+  projection: GLProjection, -- A projection / camera matrix to apply.
+  ambientColour: GLColour,  -- The ambient colour.
+  diffuseColour: GLColour,  -- The diffuse light colour.
+  ambientIntensity: Float,  -- The intensity in [0,1] of the ambient light.
+  diffuseIntensity: Float,  -- The intensity in [0,1] of the diffuse light.
+  diffuseDirection: (Float, Float, Float) -- The diffuse light direction vector.
   }
 
 -- A Zinc object views a model according to settings.
-glMeshObject : Int -> Int -> GLMesh -> GLProjection -> Element
-glMeshObject = N.glMeshObject
+glSceneObject : Int -> Int -> GLPrimModel -> GLPrimSceneView -> Element
+glSceneObject = N.glSceneObject
 
+-- Some basic matrix maths specific to 4x4 matrices...
 identity4x4 = Matrix4x4 [1,0,0,0,
                          0,1,0,0,
                          0,0,1,0,
                          0,0,0,1]
 
--- lowVal = 0-0.49
--- highVal = 0-0.25
-lowVal = 0-1.0
-highVal = 1.0
-myMesh = GLMesh {
-  nodalPositions = [Coord3D lowVal lowVal 0, Coord3D highVal lowVal 0,
-                    Coord3D lowVal highVal 0
-                    -- Coord3D highVal highVal lowVal,
-                    -- Coord3D lowVal lowVal highVal, Coord3D highVal lowVal highVal,
-                    -- Coord3D lowVal highVal highVal, Coord3D highVal highVal highVal
-                   ],
-  elements = [Triangle 0 1 2, Triangle 1 2 3, Triangle 0 1 4, Triangle 1 4 5,
-              Triangle 0 2 4, Triangle 2 4 6, Triangle 1 3 5, Triangle 3 5 7,
-              Triangle 2 3 6, Triangle 3 6 7]
-              }
+matrix4x4Row : Int -> Matrix4x4 -> [Float]
+matrix4x4Row row (Matrix4x4 [x11,x12,x13,x14,
+                             x21,x22,x23,x24,
+                             x31,x32,x33,x34,
+                             x41,x42,x43,x44]) =
+  case row of
+    0 -> [x11, x12, x13, x14]
+    1 -> [x21, x22, x23, x24]
+    2 -> [x31, x32, x33, x34]
+    3 -> [x41, x42, x43, x44]
 
-main : Element
-main = glMeshObject 500 500 myMesh identity4x4
+matrix4x4Column : Int -> Matrix4x4 -> [Float]
+matrix4x4Column row (Matrix4x4 [x11,x12,x13,x14,
+                                x21,x22,x23,x24,
+                                x31,x32,x33,x34,
+                                x41,x42,x43,x44]) =
+  case row of
+    0 -> [x11, x21, x31, x41]
+    1 -> [x12, x22, x32, x42]
+    2 -> [x13, x23, x33, x43]
+    3 -> [x14, x24, x34, x44]
+
+dotProduct : [Float] -> [Float] -> Float
+dotProduct a b = sum (zipWith (*) a b)
+
+multiply4x4 : Matrix4x4 -> Matrix4x4 -> Matrix4x4
+multiply4x4 m1 m2 =
+  let
+    p r c = dotProduct (matrix4x4Row r m1) (matrix4x4Column c m2)
+  in
+   Matrix4x4 [p 0 0, p 0 1, p 0 2, p 0 3,
+              p 1 0, p 1 1, p 1 2, p 1 3,
+              p 2 0, p 2 1, p 2 2, p 2 3,
+              p 3 0, p 3 1, p 3 2, p 3 3
+             ]
+mat4x4xv : Matrix4x4 -> [Float] -> [Float]
+mat4x4xv m v =
+  [dotProduct (matrix4x4Row 0 m) v,
+   dotProduct (matrix4x4Row 1 m) v,
+   dotProduct (matrix4x4Row 2 m) v,
+   dotProduct (matrix4x4Row 3 m) v]
+
+transpose4x4 : Matrix4x4 -> Matrix4x4
+transpose4x4 (Matrix4x4 [x11,x12,x13,x14,
+                         x21,x22,x23,x24,
+                         x31,x32,x33,x34,
+                         x41,x42,x43,x44]) =
+  Matrix4x4 [x11, x21, x31, x41,
+             x12, x22, x32, x42,
+             x13, x23, x33, x43,
+             x14, x24, x34, x44]
+
+norm4 : [Float] -> [Float]
+norm4 [x1, x2, x3, x4] = [x1/x4, x2/x4, x3/x4]
+
+quaternionToRotationMatrix (Quaternion (a, b, c, d)) =
+  Matrix4x4 [1 - 2 * (c * c + d * d), 2 * (b * c + a * d),     2 * (b * d - a * c),     0,
+             2 * (b * c - a * d),     1 - 2 * (b * b + d * d), 2 * (c * d + a * b),     0,
+             2 * (b * d + a * c),     2 * (c * d - a * b),     1 - 2 * (b * b + c * c), 0,
+             0,                       0,                       0,                       1]
+
+normaliseQuaternion (Quaternion (a, b, c, d)) =
+  let
+    norm = sqrt (a * a + b * b + c * c + d * d)
+  in
+   Quaternion (a/norm, b/norm, c/norm, d/norm)
+  
+multiplyQuaternion (Quaternion (a1, b1, c1, d1)) (Quaternion (a2, b2, c2, d2)) =
+  Quaternion (a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2,
+              c1 * d2 - d1 * c2 + a1 * b2 + a2 * b1,
+              d1 * b2 - b1 * d2 + a1 * c2 + a2 * c1,
+              b1 * c2 - c1 * b2 + a1 * d2 + d1 * a2)
+conjugateQuaternion (Quaternion (a, b, c, d)) = Quaternion (a, 0-b, 0-c, 0-d)
+
+inverseRotateVectorByQuaternion : Quaternion -> (Float, Float, Float) -> (Float, Float, Float)
+inverseRotateVectorByQuaternion q (x,y,z) =
+  let
+    Quaternion (_, x', y', z') =
+      q `multiplyQuaternion` (Quaternion (0, x, y, z)) `multiplyQuaternion` (conjugateQuaternion q)
+  in
+   (x', y', z')
+
+eulerToQuaternion theta phi psi =
+  let
+    theta2 = theta / 2
+    phi2 = phi / 2
+    psi2 = psi / 2
+    costheta2 = cos theta2
+    sintheta2 = sin theta2
+    cosphi2 = cos phi2
+    sinphi2 = sin phi2
+    cospsi2 = cos psi2
+    sinpsi2 = sin psi2
+  in
+   Quaternion (cospsi2 * costheta2 * cosphi2 + sinpsi2 * costheta2 * cosphi2,
+               sinpsi2 * costheta2 * cosphi2 - cospsi2 * sintheta2 * sinphi2,
+               cospsi2 * sintheta2 * cosphi2 + sinpsi2 * costheta2 * sinphi2,
+               cospsi2 * costheta2 * sinphi2 - sinpsi2 * sintheta2 * cosphi2)
+
+vectorToTransformMatrix (x, y, z) =
+  Matrix4x4 [1, 0, 0, x,
+             0, 1, 0, y,
+             0, 0, 1, z,
+             0, 0, 0, 1]
+
+lowVal = 0-0.8
+highVal = 0.8
+lowZ = 2
+highZ = 3.6
+
+myPrimModel =
+  let
+    -- l = left, r = right, b = bottom, t = top, f = front, a = back.
+    clbf = Coord3D lowVal lowVal lowZ
+    crbf = Coord3D highVal lowVal lowZ
+    cltf = Coord3D lowVal highVal lowZ
+    crtf = Coord3D highVal highVal lowZ
+    clba = Coord3D lowVal lowVal highZ
+    crba = Coord3D highVal lowVal highZ
+    clta = Coord3D lowVal highVal highZ
+    crta = Coord3D highVal highVal highZ
+    quad a b c d = [Triangle a b c, Triangle a c d]
+  in
+   GLPrimModel {     
+     -- Vertex ordering: anti-clockwise around external surface.
+     primitives =
+        quad clbf crbf crtf cltf ++ -- front
+        quad clba clbf cltf clta ++ -- left
+        quad crbf crba crta crtf ++ -- right
+        quad clba clta crta crba ++ -- back
+        quad cltf crtf crta clta ++ -- top
+        quad clbf crbf crba clba ++ -- bottom
+        []
+               }
+
+fovRadians = pi / 2
+
+perspectiveMatrix near far aspectRatio fovRadians =
+  let
+    cotFov = 1 / (tan (fovRadians / 2))
+  in
+   Matrix4x4 [near * cotFov / aspectRatio, 0, 0, 0,
+              0, near * cotFov, 0, 0,
+              0, 0, near/(far - near), far * near / (near - far),
+              0, 0, 1.0, 0.0]
+
+myPerspectiveMatrix : Matrix4x4
+myPerspectiveMatrix = perspectiveMatrix 1 100 1 (pi / 2)
+
+data TransformPlane = TransformXY | TransformXZ
+data CameraModifyMode = CameraRotate | CameraTransform TransformPlane
+
+type CameraMoveState = { cameraQuaternion: Quaternion, cameraTransformation: (Float, Float, Float),
+                         processedPosition: (Int, Int), mouseWasDown: Bool, 
+                         cameraModifyMode: CameraModifyMode }
+initialCameraMoveState : CameraMoveState
+initialCameraMoveState = { cameraQuaternion = Quaternion (1,0,0,0),
+                           cameraTransformation = (0, 0, 0),
+                           processedPosition = (0,0), mouseWasDown = False, 
+                           cameraModifyMode = CameraRotate }
+
+cameraMoveState : Signal CameraMoveState
+cameraMoveState = Signal.foldp updateCameraMoveState initialCameraMoveState (Signal.lift4 (,,,) Mouse.isDown Keyboard.shift Keyboard.ctrl Mouse.position)
+updateCameraMoveState : (Bool, Bool, Bool, (Int, Int)) -> CameraMoveState -> CameraMoveState
+updateCameraMoveState (mouseDown, shift, ctrl, (mouseX, mouseY) as mousePos) oldMoveState =
+  if not mouseDown
+    then {oldMoveState | mouseWasDown <- False }
+    else
+      if not oldMoveState.mouseWasDown
+        then { oldMoveState | mouseWasDown <- True, processedPosition <- mousePos,
+                              cameraModifyMode <- if shift then CameraTransform TransformXY else
+                                                     if ctrl then CameraTransform TransformXZ else CameraRotate}
+        else
+          case oldMoveState.cameraModifyMode of
+            CameraRotate ->
+              let
+                (lastX, lastY) = oldMoveState.processedPosition
+                -- Moving the mouse all the way across rotates 1 radian.
+                phi = (toFloat (mouseX - lastX)) / (toFloat canvasWidth)
+                theta = (toFloat (mouseY - lastY)) / (toFloat canvasHeight)
+                rotQuaternion = eulerToQuaternion phi 0 theta
+              in
+                { oldMoveState | cameraQuaternion <-
+                  normaliseQuaternion (rotQuaternion `multiplyQuaternion` oldMoveState.cameraQuaternion),
+                  processedPosition <- mousePos }
+            CameraTransform plane ->
+              let
+                (lastX, lastY) = oldMoveState.processedPosition
+                distanceX = (toFloat (mouseX - lastX)) / (toFloat canvasWidth)
+                distanceY = (toFloat (mouseY - lastY)) / (toFloat canvasHeight)
+                transformBy = case plane of
+                                TransformXY -> (distanceX, 0-distanceY, 0)
+                                TransformXZ -> (distanceX, 0, distanceY)
+                (otx, oty, otz) = oldMoveState.cameraTransformation
+                (tx, ty, tz) = inverseRotateVectorByQuaternion (oldMoveState.cameraQuaternion) transformBy
+              in
+                { oldMoveState |
+                    cameraTransformation <- (otx + tx, oty + ty, otz + tz),
+                    processedPosition <- mousePos }
+
+cameraMatrix : Signal Matrix4x4
+cameraMatrix = Signal.lift (\x -> quaternionToRotationMatrix x.cameraQuaternion
+                                    `multiply4x4`
+                                  vectorToTransformMatrix x.cameraTransformation) cameraMoveState
+
+canvasWidth : Int
+canvasWidth = 500
+canvasHeight : Int
+canvasHeight = 500
+
+main : Signal Element
+main = 
+  flip Signal.lift cameraMatrix (\cameraMatrixValue ->
+                                  let camPerspect = myPerspectiveMatrix `multiply4x4` cameraMatrixValue
+                                  in
+                                   -- (plainText (show (norm4 (mat4x4xv camPerspect [lowVal, lowVal, lowZ, 1.0]), norm4 (mat4x4xv camPerspect [highVal, lowVal, lowZ, 1.0]), (norm4 (mat4x4xv camPerspect [lowVal, highVal, lowZ, 1.0]))))) `above`
+                                   (glSceneObject canvasWidth canvasHeight myPrimModel 
+                                      (GLPrimSceneView { projection = transpose4x4 camPerspect,
+                                                         ambientColour = GLColour 1 1 1,
+                                                         diffuseColour = GLColour 1 0.5 0.5,
+                                                         ambientIntensity = 0.4,
+                                                         diffuseIntensity = 0.5,
+                                                         diffuseDirection = (0.3, 0.8, 0.5) })))
---
+++ Native/Graphics/WebGLScene.js
@@ -0,0 +1,174 @@
+Elm.Native.Graphics.WebGLScene = function(elm) {
+    "use strict";
+
+    var Render = ElmRuntime.use(ElmRuntime.Render.Element);
+    var JS = Elm.JavaScript(elm);
+    var D = Elm.Dict(elm);
+
+    elm.Native = elm.Native || {};
+    elm.Native.Graphics = elm.Native.Graphics || {};
+    if (elm.Native.Graphics.WebGLScene) return elm.Native.Graphics.WebGLScene;
+    
+    // Simple matrix projection...
+    var vertexShader = "attribute mediump vec3 aPos;\
+                        attribute mediump vec3 aNormal;\
+                        varying mediump vec3 normal;\
+                        uniform mediump mat4 aProjection;\
+                        void main(void) {\
+                          normal = normalize(aNormal);\
+                          gl_Position = aProjection * vec4(aPos, 1.0); \
+                        }";
+    // For now, everything is green.
+    var fragmentShader =
+        "varying mediump vec3 normal;\
+         uniform mediump vec3 aAmbientColour, aDiffuse1Colour;\
+         uniform mediump vec3 aDiffuse1Direction;\
+         uniform mediump float aAmbientIntensity, aDiffuse1Intensity;\
+         void main(void) {\
+           mediump vec3 rgb = aAmbientColour * aAmbientIntensity +\
+                      aDiffuse1Colour * aDiffuse1Intensity * \
+                      clamp(dot(normalize(normal), normalize(aDiffuse1Direction)), 0.0, 1.0);\
+           gl_FragColor = vec4(rgb, 1.0);\
+                         }"
+
+    var newElement = Elm.Graphics.Element(elm).newElement;
+    function renderGLScene(model) {
+        var jscanvas = document.createElement("canvas");
+        jscanvas.width = model.w;
+        jscanvas.height = model.h;
+
+        setTimeout(function() {
+            newContext(jscanvas);
+            updateGLScene(jscanvas, {w:0,h:0, model: null, projection: null }, model);
+        }, 100);
+        return jscanvas;
+    }
+
+    function newContext(jscanvas) {
+        var ctx = jscanvas.getContext("webgl") || jscanvas.getContext("experimental-webgl");
+        ctx.viewport(0, 0, 500, 500);
+        var shaderProgram = ctx.createProgram();
+
+        ctx.enable(ctx.DEPTH_TEST);
+
+        var vertShad = ctx.createShader(ctx.VERTEX_SHADER);
+        ctx.shaderSource(vertShad, vertexShader);
+        ctx.compileShader(vertShad);
+        ctx.attachShader(shaderProgram, vertShad);
+
+        var fragShad = ctx.createShader(ctx.FRAGMENT_SHADER);
+        ctx.shaderSource(fragShad, fragmentShader);
+        ctx.compileShader(fragShad);
+        ctx.attachShader(shaderProgram, fragShad);
+
+        ctx.linkProgram(shaderProgram);
+        ctx.useProgram(shaderProgram);
+
+        var shadeData = { aPos: ctx.getAttribLocation(shaderProgram, "aPos"),
+                          aNormal: ctx.getAttribLocation(shaderProgram, "aNormal"),
+                          aProjection: ctx.getUniformLocation(shaderProgram, "aProjection"),
+                          aAmbientColour: ctx.getUniformLocation(shaderProgram, "aAmbientColour"),
+                          aAmbientIntensity: ctx.getUniformLocation(shaderProgram, "aAmbientIntensity"),
+                          aDiffuse1Colour: ctx.getUniformLocation(shaderProgram, "aDiffuse1Colour"),
+                          aDiffuse1Direction: ctx.getUniformLocation(shaderProgram, "aDiffuse1Direction"),
+                          aDiffuse1Intensity: ctx.getUniformLocation(shaderProgram, "aDiffuse1Intensity")
+                        };
+
+        ctx.enableVertexAttribArray(shadeData.aPos);
+        ctx.enableVertexAttribArray(shadeData.aNormal);
+
+        jscanvas.shadeData = shadeData;
+        jscanvas.ctx = ctx;
+        return ctx;
+    }
+    
+    function updateGLScene(jscanvas, curr, next) {
+        jscanvas.width = next.w;
+        jscanvas.height = next.h;
+        if (jscanvas.ctx == null)
+            return;
+        jscanvas.ctx.viewport(0, 0, next.w,
+                              next.h);
+
+        var modelChanged = next.model != curr.model;
+        var sceneChanged = next.sceneview != curr.sceneview;
+        
+        if (modelChanged || sceneChanged) {
+            jscanvas.ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+            jscanvas.ctx.clearDepth(65535);
+            jscanvas.ctx.depthFunc(jscanvas.ctx.LESS);
+            jscanvas.ctx.clear(jscanvas.ctx.COLOR_BUFFER_BIT |
+                               jscanvas.ctx.DEPTH_BUFFER_BIT);
+        }
+
+        var jsmodel = next.model._0;
+
+        if (modelChanged) {
+            var jsprims = JS.fromList(jsmodel.primitives);
+            jscanvas.nNodes = jsprims.length * 3;
+            var nodalPos = new Float32Array(3 * jscanvas.nNodes);
+            var normals = new Float32Array(3 * jscanvas.nNodes);
+            for (var primID in jsprims) {
+                var thisPrim = jsprims[primID];
+                nodalPos.set([thisPrim._0._0, thisPrim._0._1, thisPrim._0._2,
+                              thisPrim._1._0, thisPrim._1._1, thisPrim._1._2,
+                              thisPrim._2._0, thisPrim._2._1, thisPrim._2._2,
+                             ], 9*primID);
+                // Compute the normal vector between edges 01 and 02 (we
+                // use an anticlockwise winding).
+                var ux = thisPrim._1._0 - thisPrim._0._0,
+                    uy = thisPrim._1._1 - thisPrim._0._1,
+                    uz = thisPrim._1._2 - thisPrim._0._2,
+                    vx = thisPrim._2._0 - thisPrim._0._0,
+                    vy = thisPrim._2._1 - thisPrim._0._1,
+                    vz = thisPrim._2._2 - thisPrim._0._2,
+                    Nx = uy * vz - uz * vy,
+                    Ny = uz * vx - ux * vz,
+                    Nz = ux * vy - uy * vx;
+                // Send the non-normalised normal vector to the shader.
+                normals.set([Nx, Ny, Nz, Nx, Ny, Nz, Nx, Ny, Nz], 9 * primID);
+            }
+
+            jscanvas.ctx.bindBuffer(jscanvas.ctx.ARRAY_BUFFER, jscanvas.ctx.createBuffer());
+            jscanvas.ctx.bufferData(jscanvas.ctx.ARRAY_BUFFER, nodalPos, jscanvas.ctx.STATIC_DRAW);
+            jscanvas.ctx.vertexAttribPointer(jscanvas.shadeData.aPos, 3, jscanvas.ctx.FLOAT, false, 0, 0);
+
+            jscanvas.ctx.bindBuffer(jscanvas.ctx.ARRAY_BUFFER, jscanvas.ctx.createBuffer());
+            jscanvas.ctx.bufferData(jscanvas.ctx.ARRAY_BUFFER, normals, jscanvas.ctx.STATIC_DRAW);
+            jscanvas.ctx.vertexAttribPointer(jscanvas.shadeData.aNormal, 3, jscanvas.ctx.FLOAT, false, 0, 0);
+        }
+
+        if (sceneChanged) {
+            var sv = next.sceneview._0;
+            jscanvas.ctx.uniformMatrix4fv(jscanvas.shadeData.aProjection, false,
+                                          JS.fromList(sv.projection._0));
+            jscanvas.ctx.uniform1f(jscanvas.shadeData.aAmbientIntensity, sv.ambientIntensity);
+            jscanvas.ctx.uniform1f(jscanvas.shadeData.aDiffuse1Intensity, sv.diffuseIntensity);
+            jscanvas.ctx.uniform3f(jscanvas.shadeData.aAmbientColour, sv.ambientColour._0,
+                                   sv.ambientColour._1, sv.ambientColour._2);
+            jscanvas.ctx.uniform3f(jscanvas.shadeData.aDiffuse1Colour, sv.diffuseColour._0,
+                                   sv.diffuseColour._1, sv.diffuseColour._2);
+            jscanvas.ctx.uniform3f(jscanvas.shadeData.aDiffuse1Direction, sv.diffuseDirection._0,
+                                   sv.diffuseDirection._1, sv.diffuseDirection._2);
+        }
+
+        if (modelChanged || sceneChanged) {
+            jscanvas.ctx.drawArrays(jscanvas.ctx.TRIANGLES, 0, jscanvas.nNodes);
+            jscanvas.ctx.flush();
+        }
+
+        return false;
+    }
+
+    function glSceneObject(w, h, model, sceneview) {
+        return A3(newElement, w, h, {
+            ctor: 'Custom',
+	    type: 'WebGL',
+	    render: renderGLScene,
+	    update: updateGLScene,
+            props: {},
+	    model: {w:w, h:h, model: model, sceneview: sceneview }
+	});
+    }
+    return elm.Native.Graphics.WebGLScene = { glSceneObject: F4(glSceneObject) };
+};
